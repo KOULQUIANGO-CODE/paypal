bdecbe4c05a84f86d6f31b884564d6888791244b
Koulquiango <kevin.orlando14042000@gmail.com>
2021-02-10 17:26:37 -0500

    version 1.0

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 
+// video 667
+
+define('URL_SITIO','http://localhost:81/paypal/');
+require 'paypal/autoload.php'; 
+$apiContext = new \PayPal\Rest\ApiContext(
+    new \PayPal\Auth\OAuthTokenCredential(
+        
+        'AYdC44aKhgt0rWF4BL2xvyQMk_GtSCFAptmjXjoxplyuD1Fl6WSIF7cPN5cxUcV48Mrbb0myIJiKSTln',// cliente DI
+        'EB586gHC2-e9uYzIh4ui9TovACpcaY6-SnFlwzk-OfOJtgsZhmmaO2aNGZQfem3ZBdGWhhEePYjwiduA'// Secret
+    )
+);
+?>
